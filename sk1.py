23c8edeca9c1a272d20148176e4f410e289c7cd5
Roman Karski <karski.roman@gmail.com>
2020-05-13 12:40:29 +0200

    plot4

--- sk1.py
+++ sk1.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-import numpy
+import numpy as np
 
 with open('energy.txt', 'r') as file:
     energy = file.read()
@@ -12,13 +12,12 @@ energy = [float(p) for p in energy if p and p[0] == '-']
 time = time.split(' ')
 time = [float(p.strip()) for p in time if p and p != '\n']
 
-plt.figure()
-plt.plot(time, energy, 'r-+')
-plt.xlabel('PWSCF[s]')
-plt.ylabel('energia całkowita [Ry]')
-plt.yscale('linear')
-plt.title('Zależność Energii całkowitej układu od czasu')
-plt.savefig('skrypt2')
+fig, ax = plt.subplots()
+ax.plot(time, np.asarray(energy, np.float32), 'r-+')
+ax.set_xlabel('PWSCF[s]')
+ax.set_ylabel('energia całkowita [Ry]')
+ax.set_title('Zależność Energii całkowitej układu od czasu')
+fig.savefig('skrypt2')
 
 print(time)
 print(energy)
