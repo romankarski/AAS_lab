79fd49954c4b546c1f0ce91885ebe9a79e24c3ae
Roman Karski <karski.roman@gmail.com>
2020-05-13 12:44:42 +0200

    plot6

--- sk1.py
+++ sk1.py
@@ -13,10 +13,11 @@ time = time.split(' ')
 time = [float(p.strip()) for p in time if p and p != '\n']
 
 fig, ax = plt.subplots()
-ax.plot(time, np.asarray(energy, np.float32), 'r-+')
+ax.plot(time, np.asarray(energy, np.float32), 'g-+')
 ax.set_xlabel('PWSCF[s]')
 ax.set_ylabel('energia całkowita [Ry]')
 ax.set_title('Zależność Energii całkowitej układu od czasu')
+plt.subplots_adjust(wspace=0.6, hspace=0.6, left=0.1, bottom=0.22, right=0.96, top=0.96)
 fig.savefig('skrypt3')
 
 print(time)
